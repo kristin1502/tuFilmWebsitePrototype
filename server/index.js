6f3204f9ac2a5cc7394d53d378b0f2f5809237fb
Kristin <kristin1502.github@gmail.com>
2025-05-07 13:33:16 +0200

    Updated corse

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import routes from "./src/routes/index.js";
 
 const app = express();
 
-app.use(cors({origin: ["http://localhost:5173/", "http://localhost:3000/", "https://tu-film-website-prototype.vercel.app/"]}));
+app.use(cors({origin: ["http://localhost:5173/", "http://localhost:3000/", "https://tu-film-website-prototype.vercel.app/", " https://tu-film-website-prototype-ofaz.vercel.app"]}));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
