9f6f25e2655a2216133e33a302d17c61790ed07c
Kristin <kristin1502.github@gmail.com>
2025-05-07 16:47:26 +0200

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -9,12 +9,24 @@ import routes from "./routes/index.js";
 const app = express();
 
 // app.use(cors({origin: ["http://localhost:5173/", "http://localhost:3000/", "https://tu-film-website-prototype.vercel.app/", " https://tu-film-website-prototype-ofaz.vercel.app", "https://tufilmwebsiteprototype-web.onrender.com/"]}));
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  'https://tu-film-website-prototype.vercel.app',
+  'https://tu-film-website-prototype-ofaz.vercel.app',
+  'https://tufilmwebsiteprototype-web.onrender.com'
+];
+
 app.use(cors({
-    origin: 'https://tufilmwebsiteprototype-web.onrender.com/',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  }));
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('CORS: Origin not allowed'));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
