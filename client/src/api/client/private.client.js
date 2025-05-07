7520b3f5d12cef48c4ea184e57a957fc701ffb5a
Kristin <kristin1502.github@gmail.com>
2025-05-07 15:24:03 +0200

    On Onbaord now

--- client/src/api/client/private.client.js
+++ client/src/api/client/private.client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import queryString from "query-string";
 
-const baseURL = "https://tu-film-website-prototype-ofaz.vercel.app/api/v1/";
+const baseURL = "https://tufilmwebsiteprototype.onrender.com/api/v1/";
 
 const privateClient = axios.create({
     baseURL,
